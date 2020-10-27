f54009a2c8b7297904a4836ac02132e9da1f2fde
Phillip Perks <p.perks@elsevier.com>
2020-10-27 13:06:34 +0000

    [cqrs-os] add integration tests for command service

--- build.sbt
+++ build.sbt
@@ -7,5 +7,5 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.11.0",
 
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  "org.scalatest" %% "scalatest" % "3.1.0" % "test"
 )
---
+++ src/test/scala/cqrsos/CommandServiceITSpec.scala
@@ -0,0 +1,69 @@
+package cqrsos
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+
+class CommandServiceITSpec extends AnyFlatSpecLike with Matchers {
+
+  it should "update the stock level and customer order event handlers using a synchronous event bus" in {
+
+    // Given
+    val eventStore = new InMemoryEventStore()
+
+    val stockLevelEventHandler = new StockLevelEventHandler
+    val customerOrdersEventHandler = new CustomerOrdersEventHandler
+
+    val eventBus = new SynchronousEventBus
+    eventBus.subscribe(stockLevelEventHandler)
+    eventBus.subscribe(customerOrdersEventHandler)
+
+    val commandService= new StockControlCommandService(eventStore, eventBus, stockLevelEventHandler)
+
+    // When
+    commandService.process(AddStockCommand(1))
+    commandService.process(AddStockCommand(1))
+    commandService.process(AddStockCommand(2))
+    commandService.process(SellStockCommand("cust-1", 2))
+    commandService.process(SellStockCommand("cust-2", 1))
+    commandService.process(SellStockCommand("cust-2", 3))
+
+    eventBus.shutdown()
+
+    // Then
+    stockLevelEventHandler.getStockCount shouldBe 1
+    customerOrdersEventHandler.getCustomerOrders("cust-1") shouldBe Seq(Order("cust-1",2,true))
+    customerOrdersEventHandler.getCustomerOrders("cust-2") shouldBe Seq(Order("cust-2",1,true), Order("cust-2",3,false))
+  }
+
+  it should "update the stock level and customer order event handlers using an asynchronous event bus" in {
+
+    // Given
+    val eventStore = new InMemoryEventStore()
+
+    val stockLevelEventHandler = new StockLevelEventHandler
+    val customerOrdersEventHandler = new CustomerOrdersEventHandler
+
+    val eventBus = new AsynchronousEventBus(2, 10)
+    eventBus.subscribe(stockLevelEventHandler)
+    eventBus.subscribe(customerOrdersEventHandler)
+
+    val commandService= new StockControlCommandService(eventStore, eventBus, stockLevelEventHandler)
+
+    // When
+    commandService.process(AddStockCommand(1))
+    commandService.process(AddStockCommand(1))
+    commandService.process(AddStockCommand(2))
+    commandService.process(SellStockCommand("cust-1", 2))
+    commandService.process(SellStockCommand("cust-2", 1))
+    commandService.process(SellStockCommand("cust-2", 3))
+
+    eventBus.shutdown()
+
+    // Then
+    stockLevelEventHandler.getStockCount shouldBe 1
+    customerOrdersEventHandler.getCustomerOrders("cust-1") shouldBe Seq(Order("cust-1",2,true))
+    customerOrdersEventHandler.getCustomerOrders("cust-2") shouldBe Seq(Order("cust-2",1,true), Order("cust-2",3,false))
+  }
+
+}
