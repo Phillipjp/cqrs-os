020630b0170d909a9c2414f9aaa8929492ebb63e
Phillip Perks <p.perks@elsevier.com>
2020-10-27 12:01:44 +0000

    [cqrs-os] remove LocalDateTime from Event

--- src/main/scala/cqrsos/StockControlCommandService.scala
+++ src/main/scala/cqrsos/StockControlCommandService.scala
@@ -6,15 +6,14 @@ import cqrsos.api.{CommandService, EventBus, EventStore}
 
 class StockControlCommandService(eventStore: EventStore, eventBus: EventBus, stockLevelEventHandler: StockLevelEventHandler) extends CommandService{
   override def process(command: Command): Unit = {
-    val now = LocalDateTime.now()
     val event = command match {
-      case c: AddStockCommand => StockAddedEvent(now, c.quantity)
+      case c: AddStockCommand => StockAddedEvent(c.quantity)
       case c: SellStockCommand =>
         if(stockLevelEventHandler.getStockCount >= c.quantity){
-          StockSoldEvent(now, c.customerId, c.quantity)
+          StockSoldEvent(c.customerId, c.quantity)
         }
         else{
-          StockNotSoldEvent(now, c.customerId, c.quantity)
+          StockNotSoldEvent(c.customerId, c.quantity)
         }
 
     }
